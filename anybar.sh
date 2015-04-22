834a5574d0f53e86a63eb4ac9f16206d2087f38f
Andrew Steele <Andrew565@gmail.com>
2015-04-22 09:43:10 -0400

    Fixed a typo in the abi alias

--- anybar.sh
+++ anybar.sh
@@ -13,7 +13,7 @@ alias ab="anybar"
 
 # List of AnyBar Icons
 alias anybar-icons="ls ~/.AnyBar/*.png | cut -d"/" -f 5 | cut -d"." -f 1"
-alias abi="anybaricons"
+alias abi="anybar-icons"
 
 # AnyBar Log
 alias anybar-log="tail -10 ~/anybar.log"

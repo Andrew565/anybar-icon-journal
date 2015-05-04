017d35ccd6498d46ca5885725e7d433d7810d6f3
Andrew Steele <Andrew565@gmail.com>
2015-05-04 16:35:31 -0400

    Added ability to specify how many log entries to return when querying for logs.

--- anybar.sh
+++ anybar.sh
@@ -16,7 +16,12 @@ alias anybar-icons="ls ~/.AnyBar/*.png | cut -d"/" -f 5 | cut -d"." -f 1"
 alias abi="anybar-icons"
 
 # AnyBar Log
-alias anybar-log="tail -10 ~/anybar.log"
+function anybar-log {
+  NUMBER=$1
+  : ${NUMBER:=9}
+  LIMIT=$(($NUMBER + 1))
+  tail -n $LIMIT ~/anybar.log
+}
 alias abl="anybar-log"
 alias anybar-log-full="cat ~/anybar.log"
 alias ablf="anybar-log-full"
